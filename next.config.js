f5052534fc13072ba3e84c2dff7fd99ed58d5e9a
Solware <solwareve@gmail.com>
2025-08-28 19:28:55 -0400

    Disable SWC and use Babel

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
-  images: {
-    unoptimized: true
-  }
+  swcMinify: false,
+  experimental: {
+    forceSwcTransforms: false,
+  },
 }
 
 module.exports = nextConfig
