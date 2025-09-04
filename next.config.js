f992227298287254c338c869411e7dfd75b75201
Taliii777 <elthon.ps4@gmail.com>
2025-09-03 21:40:39 -0400

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  distDir: 'out',
+  // output: 'export', // Comentado para Vercel
+  // distDir: 'out', // Comentado para Vercel
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
